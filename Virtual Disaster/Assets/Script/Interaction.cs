5734cdc8c0a378ec91da46a9bd9180cc0d626869
NinaNyo <40755967+NinaNyo@users.noreply.github.com>
2018-08-03 15:55:28 +0900

    인아_180803
    스쿨 씬 추가
    interaction script 수정
    인벤토리 관련 script 추가
    다운로드 받은 asset 추가

--- Virtual Disaster/Assets/Script/Interaction.cs
+++ Virtual Disaster/Assets/Script/Interaction.cs
@@ -3,41 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Interaction : MonoBehaviour {
+public class Interaction : MonoBehaviour
+{
 
-    private float gazeTime = 2f; //이 시간동안 바라봐야 이벤트 발생
 
-    private float timer;
+   // private bool gazedAt ;
+ 
+    public GameObject player;
+    pickup forPickup;
 
-    private bool gazedAt;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        /*
-		if(gazedAt)//물체를 바라보고 있으면
-        {
-            timer += Time.deltaTime;
-            if(timer>=gazeTime)//응시 시간 이상 바라보면 이벤트 실행
-            {
-                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-                timer = 0f;
-            }
-        }
-        */
-	}
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    private void Awake()
+    {
+        forPickup = player.GetComponent<pickup>();
+        //gazedAt = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       
+       
+    }
+
+
     public void onPointerEnter()
     {
-        gazedAt = true;
-        gameObject.GetComponent<MeshRenderer>().material.shader=Shader.Find("Outlined/Uniform");
+        
+        //gazedAt = true;
+        forPickup.ableto_pick = true;
+        forPickup.item_name = gameObject.name;
+        //gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Uniform");
+        gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Regular");
     }
+
     public void onPointerExit()
     {
-        gazedAt = false;
-        gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
+       // gazedAt = false;
+        forPickup.ableto_pick = false;
+        forPickup.item_name = null;
+         gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
     }
 }
---
+++ Virtual Disaster/Assets/Script/pickup.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public GameObject EventSystem;
+    inventory invScript;
+
+    public string item_name; //이름 확인
+    public bool ableto_pick; //물건이 gazed at 되어있늨가
+
+    public bool pick; //주울때와 버릴때 비교
+    public Vector3 player_place;
+
+    private GameObject Bottle_1;
+    private GameObject Candy_Bar ;
+
+    private void Awake()
+    {
+        invScript = EventSystem.GetComponent<inventory>();
+        pick = true;
+       // ableto_pick = false;
+    }
+
+
+    void Update()
+    {
+        if (ableto_pick)
+        {
+            //인벤토리 켜지기 전, 주울 때 
+            if (pick)
+            { 
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+
+                    //병 (1)
+                    if (string.Compare(item_name, "Bottle_1") == 0)
+                    {
+                        if (invScript.Bottle_1)
+                        { 
+                            invScript.Bottle_1.SetActive(true);
+                        }
+                        Bottle_1 = GameObject.Find(item_name);
+                        Bottle_1.SetActive(false);
+                    }
+
+                    //choco
+
+                    else if (string.Compare(item_name, "Candy_Bar") == 0)
+                    {
+                        if (invScript.Candy_Bar)
+                        {
+                            invScript.Candy_Bar.SetActive(true);
+                        }
+                        Candy_Bar = GameObject.Find(item_name);
+                        Candy_Bar.SetActive(false);
+                    }
+
+                
+                }
+            }
+
+            //인벤토리 켜진 상태, 즉 버릴 때
+            else if(!pick)
+            {
+                Debug.Log("trash1111");
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("trash");
+
+                    GameObject fordestroy = GameObject.Find(item_name);
+                    fordestroy.SetActive(false);
+
+                    Debug.Log(item_name.Substring(7));
+
+                    //병 (1)
+                    if (string.Compare(item_name.Substring(7), "Bottle_1") == 0)
+                    {
+                        Bottle_1.transform.position = new Vector3(player_place.x, player_place.y, player_place.z + 1);
+                        Bottle_1.SetActive(true);
+                    }
+
+                    //choco
+
+                    else if (string.Compare(item_name.Substring(7), "Candy_Bar") == 0)
+                    {
+                        Candy_Bar.transform.position = new Vector3(player_place.x, player_place.y, player_place.z + 1);
+                        Candy_Bar.SetActive(true);
+                    }
+
+
+                }
+
+            }
+        }
+
+
+    }
+
+      
+
+
+
+}
