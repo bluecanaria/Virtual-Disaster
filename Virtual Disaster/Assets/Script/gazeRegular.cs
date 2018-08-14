888ca0ab0196487f1f1ec5c3b7c66246863061be
NinaNyo <40755967+NinaNyo@users.noreply.github.com>
2018-08-15 00:30:13 +0900

    180815
    필요물건들에 셰이더 적용.
    인벤토리 위치 선정.
    줍기/버리기 코드 전부 적용

--- Virtual Disaster/Assets/Script/gazeRegular.cs
+++ Virtual Disaster/Assets/Script/gazeRegular.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class gazeRegular : MonoBehaviour {
+    public GameObject player;
+    pickup forPickup;
+
+    private void Awake()
+    {
+        forPickup = player.GetComponent<pickup>();
+        //gazedAt = false;
+    }
 
     public void onPointerEnter()
     {
+        //gazedAt = true;
+        forPickup.ableto_pick = true;
+        forPickup.item_name = gameObject.name;
+
         //gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Uniform");
         gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Regular");
     }
 
     public void onPointerExit()
     {
+        // gazedAt = false;
+        forPickup.ableto_pick = false;
+        forPickup.item_name = null;
+
         gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
     }
 }
---
+++ Virtual Disaster/Assets/gazeUniform_forblanket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gazeUniform_forblanket : MonoBehaviour {
+
+    // private bool gazedAt ;
+
+    public GameObject player;
+    pickup forPickup;
+
+    public GameObject blanket_1;
+    public GameObject blanket_2;
+
+    private void Awake()
+    {
+        forPickup = player.GetComponent<pickup>();
+        //gazedAt = false;
+    }
+
+    public void onPointerEnter()
+    {
+        //gazedAt = true;
+        forPickup.ableto_pick = true;
+        forPickup.item_name = gameObject.name;
+
+        blanket_1.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Uniform");
+        blanket_2.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Uniform");
+        //gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Outlined/Regular");
+    }
+
+    public void onPointerExit()
+    {
+        // gazedAt = false;
+        forPickup.ableto_pick = false;
+        forPickup.item_name = null;
+
+        blanket_1.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
+        blanket_2.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
+    }
+}
