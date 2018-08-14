a4e0ec88b50228da32a1a58f1218ae934f22dae8
CallieKim <calliekl@naver.com>
2018-08-14 15:53:19 +0900

    180814_PM3

---
+++ Virtual Disaster/Assets/Script/JHK/MenuBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBack : MonoBehaviour {
+
+    public GameObject Title;
+    public GameObject ThisMenu;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Title.SetActive(true);
+            ThisMenu.SetActive(false);
+        }
+
+    }
+}
