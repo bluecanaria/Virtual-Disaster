41ebfef89761ab44f16c6c67d0d30225bb5c5ed1
NinaNyo <40755967+NinaNyo@users.noreply.github.com>
2018-09-07 17:17:36 +0900

    180907 신인아
    loading 화면 추가

--- Virtual Disaster/Assets/Script/JHK/SceneChange.cs
+++ Virtual Disaster/Assets/Script/JHK/SceneChange.cs
@@ -2,14 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneChange : MonoBehaviour {
 
-    public int SceneNum;
+public class SceneChange : MonoBehaviour
+{
+    AsyncOperation async_operation;
+    public Image loading;
 
-	// Use this for initialization
-	void Start () {
-        SceneManager.LoadScene(SceneNum);
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartLoad("modified_schoolScene"));
+        // SceneManager.LoadScene("LoadingScene");
     }
-	
+
+    //public void onClick()
+    //{
+    //    SceneManager.LoadScene("LoadingScene");
+
+    //    //LoadingScene
+    //    //modified_schoolScene
+    //}
+
+
+    public IEnumerator StartLoad(string strSceneName)
+    {
+        loading.gameObject.SetActive(true);
+
+        async_operation = SceneManager.LoadSceneAsync(strSceneName);
+        //async_operation.allowSceneActivation = false;
+        while (!async_operation.isDone)
+        {
+            yield return null;
+        }
+        //if (IsDone == false)
+        //{
+        //    IsDone = true;
+
+        //    while (async_operation.progress < 0.9f)
+        //    {
+
+        //        yield return true;
+        //    }
+        //}
+        yield return null;
+    }
+
 }
+
